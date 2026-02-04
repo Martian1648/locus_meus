517e18e2b10fa260b457bb2370d3ed9bc55e8567
Martian1648 <Martian1648@outlook.com>
2026-02-04 10:12:29 -0600

    Deleted old files, added the new ones, added vuetify, & other changes

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,33 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref, computed} from 'vue';
+import home from './components/home.vue'
+import about from './components/about.vue'
+
+const routes = {
+  '/': home,
+  '/about': about,
+}
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', e => {
+  currentPath.value = window.location.hash;
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <p>Sigma</p>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <nav>
+    <a href="#/">Home</a>
+    <a href="#/about">About</a>
+  </nav>
+  <h1>Locus Meus</h1>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <component :is="currentView" />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import {aliases, mdi} from 'vuetify/iconsets/mdi'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+    icon:{
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+})
+createApp(App).use(vuetify).mount('#app')
