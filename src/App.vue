2cfd8359d0a31f195a1a485fe4de6c7e1c38e8ab
Martian1648 <Martian1648@outlook.com>
2026-02-28 10:54:00 -0600

    homepage update

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,58 @@ window.addEventListener('hashchange', e => {
 const currentView = computed(() => {
   return routes[currentPath.value.slice(1) || '/'] || NotFound
 })
+
+const navItems = [
+  {
+    title: 'Home',
+    link: '#/',
+    icon: 'mdi-home',
+    show:false,
+  },
+  {
+    title: 'About',
+    link: '#/about',
+    icon: 'mdi-information',
+    show:false,
+  }
+]
+
+const showItems = ref(false)
 </script>
 
 <template>
-  <nav>
-    <a href="#/">Home</a>
-    <a href="#/about">About</a>
-  </nav>
-  <h1>Locus Meus</h1>
+  <v-app>
+<!--  <nav>-->
+<!--    <a href="#/">Home</a>-->
+<!--    <a href="#/about">About</a>-->
+<!--  </nav>-->
+<!--  <h1>Locus Meus</h1>-->
+  <v-navigation-drawer permanent width="200"
+                       @mouseenter="showItems = true"
+  @mouseleave="showItems = false"
+  color="green-darken-3" class="text-blue-grey-lighten-5">
 
-  <component :is="currentView" />
+    <v-list-item-title  class="text-center text-h6" >LuTheologus</v-list-item-title>
+
+    <v-divider></v-divider>
+    <div v-for="item in navItems" :key="item.title">
+      <v-list-item link :href="item.link" >
+        <v-row>
+          <v-col>
+        <v-icon >{{ item.icon }}</v-icon>
+          </v-col>
+          <v-col>
+            <v-list-item-title v-if="showItems">{{ item.title }}</v-list-item-title>
+          </v-col>
+        </v-row>
+      </v-list-item>
+    </div>
+  </v-navigation-drawer>
+
+  <v-main>
+    <component :is="currentView" />
+  </v-main>
+  </v-app>
 </template>
 
 <style scoped>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,10 +1,69 @@
 <script setup >
+import {pl} from "vuetify/locale";
 
+const toGo = [
+  {
+    title:"The Book of Concord",
+    img: "Concordia.jpg",
+    desc:"Read the Lutheran Confessions, those symbols that accurately" +
+        " exposit the Christian faith according to the Scriptures, and thus" +
+        " set the borders of orthodoxy.",
+    link:"/",
+    position: "center 7%"
+  },
+  {
+    title:"Translated Works",
+    img: "Jerome.jpg",
+    desc:"The Lutheran dogmatic tradition is filled with valuable works, " +
+        "some of which have been translated (professionally or otherwise) into" +
+        " english.",
+    link:"/",
+    position: "center 20%"
+  },
+  {
+    title:"Untranslated Works",
+    img: "Templum.jpg",
+    desc:"Many important works from our theologians are still found only in Latin or German, nevertheless " +
+        "they can be valuable to look at for those who can read those languages.",
+    link:"/",
+    position: "center 7%"
+  }
+]
 </script>
 
 <template>
-<h1>Domo</h1>
-<v-icon icon="mdi-home"></v-icon>
+  <v-defaults-provider :defaults="{ VBtn: { variant: 'outlined', color: '#eee' } }">
+<v-carousel
+direction="vertical"
+class="h-screen"
+vertical-arrows="right"
+hide-delimiters
+crossfade=true
+next-icon="mdi-menu-right"
+prev-icon="mdi-menu-left">
+  <v-carousel-item v-for="place in toGo" :key="place.title"
+  :src="place.img" cover :position="place.position">
+    <div class="d-flex-inline fill-height justify-center align-center
+     text-white pa-10" style="background-color:rgba(38, 50, 56, .9); " >
+      <h1 class="text-display-large text-center">
+        {{place.title}}
+      </h1>
+      <v-divider></v-divider>
+      <div class="pa-5">
+        <v-container>
+          <v-row>
+            <v-col>{{place.desc}}</v-col>
+            <v-col>
+            <v-chip variant="outlined" link ripple=false append-icon="mdi-arrow-top-right">
+              Go to {{place.title}}
+            </v-chip></v-col>
+          </v-row>
+        </v-container></div>
+    </div>
+  </v-carousel-item>
+
+</v-carousel>
+  </v-defaults-provider>
 </template>
 
 <style scoped>
