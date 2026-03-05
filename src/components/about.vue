af8b2601092d3b6c1e468c9ab0b323d799ac726a
Martian1648 <Martian1648@outlook.com>
2026-03-04 22:20:45 -0600

    yea

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-<h1>About</h1>
+<h1>De Hoc Sito</h1>
+<v-expansion-panel
+  title="Propter quid?"
+  text="The proximate external motivating cause (id est the 'propter quid') is that I am doing this for a class."></v-expansion-panel>
+  <v-expansion-panel
+    title="Cur?"
+    text="The proximate internal motivating cause (id est the 'cur') is that I have an enormous love for theology and for the works that have been translated, and I wanted them to be shown."></v-expansion-panel>
+  <v-expansion-panel
+    title="Quae est causa efficiens?"
+    text="Remotely, God, as he is the first mover of all things. However, proximately, the efficient cause is me, myself, & I"></v-expansion-panel>
+  <v-expansion-panel
+    title="De me"
+    text="I am absolutely not putting that on the world wide web. ">
+
+  </v-expansion-panel>
 </template>
 
-<style scoped>
-
-</style>
---
+++ src/components/theologians.vue
@@ -0,0 +1,82 @@
+<script setup>
+const theologians = [
+  {
+    "name":"Martin Luther",
+    "birth":"1483",
+    "death":"1546",
+    "location":"Wittenberg",
+    "occupation":"Professor of Theology",
+    "link":"https://en.wikipedia.org/wiki/Martin_Luther"
+  },
+  {
+    "name":"Philipp Melanchthon",
+    "birth":"1497",
+    "death":"1560",
+    "location":"Wittenberg",
+    "occupation":"Professor of Greek and Reformer",
+    "link":"https://en.wikipedia.org/wiki/Philipp_Melanchthon"
+  },
+  {
+    "name":"Martin Chemnitz",
+    "birth":"1522",
+    "death":"1586",
+    "location":"Braunschweig",
+    "occupation":"Theologian and Superintendent",
+    "link":"https://en.wikipedia.org/wiki/Martin_Chemnitz"
+  },
+  {
+    "name":"Johann Gerhard",
+    "birth":"1582",
+    "death":"1637",
+    "location":"Jena",
+    "occupation":"Professor of Theology",
+    "link":"https://en.wikipedia.org/wiki/Johann_Gerhard"
+  },
+  {
+    "name":"Aegidius Hunnius",
+    "birth":"1550",
+    "death":"1603",
+    "location":"Wittenberg",
+    "occupation":"Professor of Theology",
+    "link":"https://en.wikipedia.org/wiki/Aegidius_Hunnius"
+  },
+  {
+    "name":"Leonhard Hutter",
+    "birth":"1563",
+    "death":"1616",
+    "location":"Wittenberg",
+    "occupation":"Professor of Theology",
+    "link":"https://en.wikipedia.org/wiki/Leonhard_Hutter"
+  },
+  {
+    "name":"Abraham Calov",
+    "birth":"1612",
+    "death":"1686",
+    "location":"Wittenberg",
+    "occupation":"Professor of Theology",
+    "link":"https://en.wikipedia.org/wiki/Abraham_Calovius"
+  },
+  {
+    "name":"Johann Franz Buddeus",
+    "birth":"1667",
+    "death":"1729",
+    "location":"Jena",
+    "occupation":"Professor of Theology",
+    "link":"https://en.wikipedia.org/wiki/Johann_Franz_Buddeus"
+  },
+  {
+    "name":"Carl Ferdinand Wilhelm Walther",
+    "birth":"1811",
+    "death":"1887",
+    "location":"St. Louis",
+    "occupation":"Pastor and Theologian",
+    "link":"https://en.wikipedia.org/wiki/C._F._W._Walther"
+  }
+]
+
+</script>
+
+<template>
+
+</template>
+
