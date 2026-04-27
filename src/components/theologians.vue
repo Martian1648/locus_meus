ae0062d763835eaf55fed2a93f8a7193c50b8fb7
Martian1648 <Martian1648@outlook.com>
2026-04-27 09:44:48 -0500

    change to get thingy thing thang

--- src/components/theologians.vue
+++ src/components/theologians.vue
@@ -11,7 +11,7 @@ async function loadTheologians() {
   error.value = "";
 
   try {
-    const response = await fetch("http://localhost:3000/authors")
+    const response = await fetch("/api/authors")
     if(!response.ok) {
       throw new Error('failed to load authors')
     }
